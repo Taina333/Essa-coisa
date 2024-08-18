3f552c053e5423e48513515932dc2cfa067602a9
Taina333 <00001107574183SP@al.educacao.sp.gov.br>
2024-08-17 23:55:36 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Essa-coisa
+# Essa-coisa
---
+++ assets/js/login.js
@@ -0,0 +1,58 @@
+const usuarios = [
+    {
+        usuario: 'ksora',
+        senha: '1234'
+    },
+    {
+        usuario: 'Suzany',
+        senha: '6670'
+    },
+    {
+        usuario: 'Caleb',
+        senha: '3066'
+    }
+]
+
+/* login com sucesso */
+document.getElementById('formulario').addEventListener('submit',function login(event){
+    event.preventDefault();
+
+    var user = document.getElementById('user').value;
+    var senha = document.getElementById('senha').value;
+    var loginValido = false;
+
+    for (var i in usuarios){
+        if (user == usuarios[i].usuario && senha == usuarios[i].senha){
+            loginValido = true;
+            break;
+    }
+}
+
+    if(loginValido == true){
+        alert('Sucesso');
+        location.href = "comentarios.html";
+    }else{
+/* login informação incorreta */
+        alert('Usuário ou senha incorreta');
+    }});
+
+/* registrar erro */
+document.getElementById('registrar').addEventListener('click', function() {
+    var user = document.getElementById('user').value;
+    var senha = document.getElementById('senha').value;
+
+    if (!user || !senha) {
+        alert('Preencha ambos os campos para registrar');
+        return;
+    }
+/* registrar usuário existente */
+    var usuarioExistente = usuarios.some(u => u.usuario === user);
+
+    if (usuarioExistente) {
+        alert('Usuário já registrado');
+    } else {
+/* registrar com sucesso */
+        usuarios.push({ usuario: user, senha: senha });
+        alert('Usuário registrado com sucesso');
+    }
+    });
